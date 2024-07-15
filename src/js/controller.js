f447f693c8f12ad7b107288774ff4cb434759f02
KESHAV4820 <keshav48kumar@gmail.com>
2024-07-15 12:28:37 +0530

    after shifting the frontendcode to desktopZ2SSCCGO

--- src/js/controller.js
+++ src/js/controller.js
@@ -236,7 +236,7 @@ async function fetchRecordCount(parameterObjData) {
 // New async function to fetch venue statistics
 async function fetchVenueStat(parameterObjData) {
   try {
-    const response = await fetch('http://127.0.0.1:3000/api/v1/venuerecords?limit=1170000&offset=0', {
+    const response = await fetch('http://127.0.0.1:3000/api/v1/venuerecords?limit=2000000&offset=0', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
